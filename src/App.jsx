0d1647bee4b660f066c48800bae9f1e6f08b6ad6
Shubham Ambure <sambure09@gmail.com>
2025-02-17 15:10:23 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import LandingPage from "./components/LandingPage.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./components/about/About.jsx";
 
 const App = () => {
   return (
     <>
       <LandingPage />
+      <About />
     </>
   );
 };
